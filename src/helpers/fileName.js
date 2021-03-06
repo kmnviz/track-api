df106e997c6e3c0ad52b12494828f8443b085204
ivan.kamenov <ivan.kamenov@takeaway.com>
2021-03-06 23:31:26 +0200

    Remove helpers dir for now

--- src/helpers/fileName.js
+++
@@ -1,8 +0,0 @@
-const getUniqueId = require('./getUniqueId');
-
-module.exports = (fileName, fileExt = '') => {
-    const fileNameParts = fileName.split('.');
-    const fileExtension = fileExt === '' ? fileNameParts[fileNameParts.length - 1] : fileExt;
-
-    return getUniqueId('', 8) + '.' + fileExtension;
-};
--- src/helpers/uniqueId.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-
-module.exports = (prefix, bytes) => {
-    return `${prefix}${crypto.randomBytes(bytes).toString('hex')}`;
-};
