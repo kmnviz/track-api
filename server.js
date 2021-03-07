35a5f3cae6d8c53ba3d06e7d70e87705877a7db8
ivan.kamenov <ivan.kamenov@takeaway.com>
2021-03-07 21:24:18 +0200

    Clear master branch

--- server.js
+++ server.js
@@ -1,31 +1,9 @@
-const path = require('path');
-const crypto = require('crypto');
 const express = require('express');
 const app = express();
 const port = 3030;
 
-const uniqueId = (bytes) => {
-    return `t-${crypto.randomBytes(bytes).toString('hex')}`;
-};
-
-app.use('/public', express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
     res.json({ message: 'hello', route: '/' });
-});
-
-app.post('/test', (req, res) => {
-    res.json({ message: 'hello', route: '/test' });
-});
-
-app.post('/upload/:user_id', (req, res) => {
-    const userId = req.params['user_id'];
-    const trackId = uniqueId(8);
-    const upload = require('./upload')(userId, trackId);
-
-    upload(req, res, (err) => {});
-
-    res.json({ message: 'hello', route: '/upload', trackId: trackId });
 });
 
 app.listen(port, () => {
